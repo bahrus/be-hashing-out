deaf51cc3fc671137c6f1c6053eb278e18c22495
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-02 18:36:35 -0400

    Initial commit

--- register.js
+++ register.js
@@ -1,9 +1,19 @@
 //TODO:  store in indexdb?
 //Especially if done in service worker, via HTMLRewriter, [w3c willing](https://github.com/whatwg/dom/issues/1222)
 /**
+ * @type Set<string>
+ */
+const registry = new Set();
+/**
  * 
  * @param {string | Array<string>} hash 
  */
 export function register(hash){
-    if()
+    if(Array.isArray(hash)){
+        for(const aHash of hash){
+            registry.add(aHash)
+        }
+    }else{
+        registry.add(aHash);
+    }
 }
