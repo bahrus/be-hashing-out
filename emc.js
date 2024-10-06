88204ff1d5eb15fbea8d64ae35d35639f17c4925
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-06 09:41:05 -0400

    0.0.1

--- emc.js
+++ emc.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { BeHive, seed, MountObserver } from 'be-hive/be-hive.js';
 import { Registry } from 'be-hive/Registry.js';
-/** @import {EMC, EventListenerOrFn} from './ts-refs/trans-render/be/types.d.ts' */
+/** @import {EMC, EventListenerOrFn} from './ts-refs/trans-render/be/types' */
 /** @import {Actions, PAP,  AP} from './ts-refs/be-hashing-out/types' */;
 /** @import {CSSQuery} from './ts-refs/trans-render/types.js' */
 
